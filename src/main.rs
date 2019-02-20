21b93f547713aae291e1923973ae98cec11dd4d6
Oscar Nylander <oscar@hedvig.com>
2019-02-20 19:54:24 +0100

    Write to files as well

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ extern crate serde_derive;
 use graphql_client::{GraphQLQuery, Response};
 use reqwest;
 
+use std::fs;
+use std::io::prelude::*;
+
 #[derive(GraphQLQuery)]
 #[graphql(
     schema_path = "src/graphql/schema.json",
@@ -18,7 +21,6 @@ struct TextkeysQuery;
 
 fn main() {
     let query = TextkeysQuery::build_query(textkeys_query::Variables {});
-    dbg!(&query);
     let res: Response<textkeys_query::ResponseData> = reqwest::Client::new()
         .post("https://graphql.dev.hedvigit.com/graphql")
         .json(&query)
@@ -34,17 +36,24 @@ fn main() {
     let data = res.data.unwrap();
     for language in data.languages {
         let language = language.unwrap();
-        let filename = format!("res/values-{}/strings.xml", language.code);
-        dbg!(filename);
-        //dbg!(language);
-        let mut buffer = String::from(r#"
-<?xml version="1.0" encoding="utf-8"?>
-<resources>"#);
+        let directory = format!("output/res/values-{}", language.code);
+        fs::create_dir_all(&directory).unwrap();
+        let mut buffer = String::from(
+            r#"<?xml version="1.0" encoding="utf-8"?>
+<resources>
+"#,
+        );
         let translations = language.translations.unwrap();
         for translation in translations {
-            buffer.push_str(&format!("<string name=\"{}\">{}</string>", translation.key.unwrap().value, translation.text));
+            buffer.push_str(&format!(
+                r#"    <string name="{}">{}</string>
+"#,
+                translation.key.unwrap().value,
+                translation.text
+            ));
         }
         buffer.push_str("</resources>");
-        println!("{}", buffer);
+        let mut file = fs::File::create(format!("{}/strings.xml", directory)).unwrap();
+        file.write_all(buffer.as_bytes()).unwrap();
     }
 }
